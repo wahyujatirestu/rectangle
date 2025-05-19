d2b4f331fc3bc35e52c9a057ba4c42555d8bec47
Restu Adi Wahyujati <adijati1029@gmail.com>
2025-05-19 23:14:39 +0700

    fix: update module path

--- shape/main.go
+++ shape/main.go
@@ -10,5 +10,5 @@ func (rectangle *Rectangle) Area()  float32{
 }
 
 func (rectangle *Rectangle) Perimeter() float32 {
-	return 2 * (rectangle.Width + rectangle.Length)
+	return 2 * (rectangle.Width + rectangle.Length	)
 }
